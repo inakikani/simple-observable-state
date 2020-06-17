a721cefcb363c939c159fa16af7bb41e33047280
inakikani <inakikaninaki@gmail.com>
2020-06-17 18:19:08 +0200

    build with tsc

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import {ACTION_SUBJECT} from './observables'
 
 // create a state observable if it does not already exists
-export {createRxState as rxState} from './create-rx-state'
+export {createRxState as state$$} from './create-rx-state'
 
 // dispatch and action
 export const dispatch = ACTION_SUBJECT.next.bind(ACTION_SUBJECT)
