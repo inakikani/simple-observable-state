fa67364e5f240ec972dfa9a8009f024925e523c4
inakikani <inakikaninaki@gmail.com>
2021-02-28 00:26:49 +0100

    passes 10 tests

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,106 @@
-import {ACTION_SUBJECT} from './observables'
-import {createRxState} from './create'
-/**
- * dispatch action or sideEffect to all registered reducers.
- * @param action the action to be dispatched, must be either a valid action
- */
-export const dispatch = ACTION_SUBJECT.next.bind(ACTION_SUBJECT)
-export {createRxState as create} from './create'
-export default createRxState
+import { BehaviorSubject, from, Observable } from "rxjs";
+import { pluck } from 'rxjs/operators'
+import produce, { nothing } from 'immer'
 
+export function get(path, obj) {
+    return path.reduce((src, str) => src?.[str] ?? null, obj)
+}
+
+type ObservableStateOptions = {
+    id: string;
+    source?: ObservableState<any>,
+    async?: Promise<any>,
+    pluck?: string[]
+}
+
+export let observableStatesMap = new Map()
+
+export class ObservableState<T> extends BehaviorSubject<T> {
+
+    id: string
+
+    _source?: ObservableState<T>
+    _pluck?: string[]
+
+    constructor(init: any, options?: ObservableStateOptions) {
+        super(init)
+        if (options?.id) {
+            this.id = options.id
+        } else {
+            this.id = JSON.stringify(init)
+        }
+        if (options?.source) {
+            this._source = options.source
+            if (options?.pluck && options.pluck.length !== 0) {
+                this._pluck = Array.isArray(options.pluck) ? options.pluck : [options.pluck]
+                this._source
+                    .pipe(
+                        pluck(...this._pluck)
+                    ).subscribe({
+                        next: (s) => super.next(s as T),
+                        error: err => super.error(err),
+                        complete: () => super.complete()
+                    })
+            } else {
+                this._source.subscribe({
+                    next: (s) => super.next(s as T),
+                    error: err => super.error(err),
+                    complete: () => super.complete()
+                })
+            }
+
+        }
+    }
+
+    path<S>(path: string | string[]): ObservableState<S> {
+        if (!path || path.length === 0) { return this as ObservableState<any> }
+        const pathArr = Array.isArray(path) ? path : [path]
+        const childrenID = `${this._source?.id ?? ''}:${pathArr.toString()}`
+
+        const initialPathState = get(pathArr, this.getValue())
+        return new ObservableState<S>(initialPathState, {
+            id: childrenID,
+            source: this,
+            pluck: pathArr
+        })
+    }
+
+    subscribe(...args: any[]) {
+        return super.subscribe(...Array.from(arguments))
+    }
+
+    next(nState: any, path?: string | string[]) {
+        const pathArr = path?.length > 0
+            ? Array.isArray(path) ? path : [path]
+            : []
+        const fullPathArr = [...(this?._pluck ?? []), ...pathArr]
+
+        if(this._source){
+            this._source.next(nState, fullPathArr)
+        } else {
+            let newState = produce(this.getValue(), draft => {
+                if(pathArr?.length === 0){
+                    return nState ?? nothing
+                } else {
+                    let propName = pathArr.pop()
+                    get(pathArr, draft)[propName] = nState
+                }
+            })
+            super.next(newState as T)
+        }
+    }
+}
+
+export function sos<T>(id: string, init: any, options?: ObservableStateOptions): ObservableState<T> {
+    if (!observableStatesMap.has(id)) {
+        observableStatesMap.set(id, new ObservableState(init, {
+            id: id,
+            ...options,
+        }))
+    }
+
+    console.log('states', observableStatesMap)
+    return observableStatesMap.get(id)
+}
+
+export default sos
--- test/observableState.test.js
+++ test/observableState.test.js
@@ -1,110 +1,130 @@
-const {
-    createRxState,
-    toObservableState
-} = require('../src/create')
-const {
-    timeoutWith,
-    take
-} = require('rxjs/operators')
-const {
-    of
-} = require('rxjs')
-const {dispatch} = require('../src')
+const { BehaviorSubject, Subject } = require("rxjs")
+const { take } = require("rxjs/operators")
+const { ObservableState } = require("../src")
 
-describe('toObservableState: returns an ObservableState', () => {
-    let _os$;
-    test('toObservableState does not throw', () => {
-        expect(() => {
-            _os$ = toObservableState(of(1))
+describe('creating ObservableState', () => {
+    test('no params', () => {
+        expect(()=>{
+            new ObservableState()
         }).not.toThrow()
     })
-    test('returned ObservableState is a Subscribable', () => {
-        expect(() => {
-            const sub = _os$.subscribe()
-            sub.unsubscribe()
+
+    test('new Obs with an initial state, no options', done => {
+        expect(()=>{
+            let x = new ObservableState({a:{b:'foo'}})
+            console.log(x)
         }).not.toThrow()
+        done()
     })
-    test('returned ObservableState has a path method', () => {
-        expect(_os$).toHaveProperty('path')
-        expect(typeof _os$.path).toBe('function')
+    test('first value emitted', done => {
+        const _state = {a:{b:'foo'}}
+        const state = new ObservableState(_state)
+        state.subscribe(s => {
+            expect(s).toBe(_state)
+            expect(s.a.b).toEqual('foo')
+            done()
+        })
     })
-    test('', () => {})
+    test('test path method, no param', done => {
+        const _state = {a:{b:'foo'}}
+        const state = new ObservableState(_state).path()
+        state.subscribe(s => {
+            expect(s).toBe(_state)
+            expect(s.a.b).toEqual('foo')
+            done()
+        })
+    })
+    test('test path method, with string path', done => {
+        const _state = {a:{b:'foo'}}
+        const state = new ObservableState(_state).path('a')
+        state.subscribe(s => {
+            expect(s).toBe(_state.a)
+            expect(s.b).toEqual('foo')
+            done()
+        })
+    })
+    test('test path method, with array path', done => {
+        const _state = {a:{b:'foo'}}
+        const state = new ObservableState(_state).path(['a', 'b'])
+        state.subscribe(s => {
+            expect(s).toBe(_state.a.b)
+            expect(s).toEqual('foo')
+            done()
+        })
+    })
 })
 
-
-describe('ObservableState.path: subscribe to path', () => {
-    const stateName = 'pathTestState'
-    const reducer = (state, action) => {
-        return action.payload
-    }
-    const initialState = {
-        path: {value: 'pathed value'}
-    }
-    const state$ = createRxState(stateName, reducer, initialState)
-
-    test('path with no argument returns source observable', done => {
-        state$
-            .path()
-            .pipe(take(1))
-            .subscribe(value => {
-                expect(value).toEqual(initialState)
-                expect(value).toBe(initialState)
+describe('emitting new state', () => {
+    let _state = {a: {b: 'foo'}, c: 200}
+    
+    test('set state to undefined', done => {
+        let next = jest.fn()
+        let s = new ObservableState({..._state})
+        s.pipe(take(2)).subscribe({
+            next: next,
+            complete: () => {
+                expect(next).toHaveBeenCalledTimes(2)
+                expect(next).toHaveBeenNthCalledWith(2, undefined)
                 done()
-            })
-    })
-
-    test('path returns observable of state property', done => {
-        state$
-            .path('path.value')
-            .pipe(take(1))
-            .subscribe(
-                value => {
-                    expect(value).toBe('pathed value')
-                },
-                done,
-                done
-            )
-    })
-    test('path value is not emitted if it has not changed', done => {
-        const next = jest.fn()
-        const nextState = jest.fn()
-        const state1 = {
-            path: {value: 'changed value'}
-        }
-        const state2 = {
-            path: {value: 'changed value'},
-            aside: 42
-        }
-
-        state$
-            .path('path.value')
-            .pipe(timeoutWith(2500, of('done')))
-            .subscribe(
-                next,
-                done,
-                () => {
-                    expect(next).toHaveBeenNthCalledWith(1, 'pathed value')
-                    expect(next).toHaveBeenNthCalledWith(2, 'changed value')
-                    expect(next).not.toHaveBeenNthCalledWith(3, 'changed value')
-                    expect(next).toHaveBeenLastCalledWith('done')
-                    // whilst the state should have emitted for initial subscribe and 2 dispatched actions
-                    expect(nextState).toHaveBeenCalledTimes(3)
-                    expect(nextState).toHaveBeenLastCalledWith(state2)
-                    done()
-                }
-            )
-
-        state$.subscribe(
-            nextState
-        )
-        
-        dispatch({
-            type: 'change/value', 
-            payload: state1
+            }
         })
-        dispatch({
-            type: 'change/state', 
-            payload: state2
+        s.next()
+    })
+    test('set state to new state', done => {
+        let next = jest.fn()
+        let newState = {
+            ..._state,
+            c: 400,
+            d: 'bar'
+        }
+        let s = new ObservableState({..._state})
+
+        s.pipe(take(2)).subscribe({
+            next: next,
+            complete: () => {
+                expect(next).toHaveBeenCalledTimes(2)
+                expect(next).toHaveBeenNthCalledWith(1, _state)
+                expect(next).toHaveBeenNthCalledWith(2, newState)
+                done()
+            }
         })
+        s.next(newState)
+    })
+    test('set state prop to undefined via path', done => {
+        let next = jest.fn()
+        let s = new ObservableState({..._state}).path('c')
+        s.pipe(take(2)).subscribe({
+            next: next,
+            complete: () => {
+                expect(next).toHaveBeenCalledTimes(2)
+                expect(next).toHaveBeenNthCalledWith(1, 200)
+                expect(next).toHaveBeenNthCalledWith(2, undefined)
+                done()
+            }
+        })
+        s.next()
+    })
+    test('set state deep prop to new value via path', done => {
+        let next = jest.fn()
+        let s = new ObservableState({..._state}).path('c')
+        s.pipe(take(3)).subscribe({
+            next: next,
+            complete: () => {
+                expect(next).toHaveBeenCalledTimes(3)
+                expect(next).toHaveBeenNthCalledWith(1, 200)
+                expect(next).toHaveBeenNthCalledWith(2, undefined)
+                expect(next).toHaveBeenNthCalledWith(3, 300)
+                done()
+            }
+        })
+        s.next()
+        s.next(300)
     })
 })
+
+// describe('test sandpit, used for anything', () => {
+//     test('subject on subscribe', done => {
+//         let s = new BehaviorSubject(1)
+//         let o1 = s.subscribe()
+//     })
+// })
