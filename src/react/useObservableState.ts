dfeeb563d4ff7d429d9388f94ec6a00b83a4c0dd
inakikani <inakikaninaki@gmail.com>
2021-04-03 16:00:14 +0200

    hook in ts file

--- src/react/useObservableState.ts
+++ src/react/useObservableState.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { ObservableState } from ".."
 
-export function useObservableState(state: any){
+export default function useObservableState(state: any){
     const [stateValue, setStateValue] = useState(state instanceof ObservableState ? state.getValue() : state)
     const stateObs = state instanceof ObservableState
         ? state
